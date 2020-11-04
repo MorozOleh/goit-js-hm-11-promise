e5f50e7c167bc731a9f7810a6bb101c7f1b765e0
morozoleh <olegmoroz93@gmail.com>
2020-11-04 17:14:30 +0200

    commite-fifth

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-import task1 from ' ./js/task1.js';
-import task2 from './js/task2.js';
-import task3 from './js/task3.js';
+import { logger } from './js/task1.js'
+import { task2 } from './js/task2.js'
+import { logSuccess, logError } from './js/task3.js';
