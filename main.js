33524818067bdadbfcc351871ec81c8645613fd1
morozoleh <olegmoroz93@gmail.com>
2020-11-04 17:52:03 +0200

    fifth

--- main.js
+++ main.js
@@ -2,4 +2,4 @@ import "./javaScript/task1.js";
 import "./javaScript/task2.js";
 import "./javaScript/task3.js";
 
-console.log('hello teacher');
+console.log('%c hello teacher ', 'background: yellow; color: black; border-radius: 20px; font-size:30px');
