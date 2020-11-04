fe068aefd9039d37625bb475ab12f4291ead4970
morozoleh <olegmoroz93@gmail.com>
2020-11-04 17:40:14 +0200

    second

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { logger } from './js/task1.js';
-import { task2 } from './js/task2.js';
-import { logSuccess, logError } from './js/task3.js';
+import "./js/task1.js";
+import "./js/task2.js";
+import "./js/task3.js";
 
 console.log('hello teacher');
